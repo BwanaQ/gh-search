b0f955384fd5e7dcbb696eb991c7ea142e546f98
BwanaQ <thunjawax@gmail.com>
2021-02-12 12:48:27 +0300

    add repository view and controller

--- src/app/components/repository/repository.component.ts
+++ src/app/components/repository/repository.component.ts
@@ -1,15 +1,26 @@
+import { ProfileService } from './../../services/profile.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-repository',
   templateUrl: './repository.component.html',
-  styleUrls: ['./repository.component.css']
+  styleUrls: ['./repository.component.css'],
 })
 export class RepositoryComponent implements OnInit {
+  repositories: any[] = [];
+  repositoryName: string = 'hekalu';
 
-  constructor() { }
+  constructor(private profileService: ProfileService) {}
 
-  ngOnInit(): void {
+  findRepository() {
+    this.profileService.updateRepository(this.repositoryName);
+    this.profileService.searchRepositories().subscribe((repository: any) => {
+      this.repositories = repository.items;
+      console.log(this.repositories);
+    });
   }
 
+  ngOnInit(): void {
+    this.findRepository();
+  }
 }
