3031a36c69205b745dd5e00329c4e56bdef77011
BwanaQ <thunjawax@gmail.com>
2021-02-12 11:46:38 +0300

    create profile component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DurationPipe } from './pipes/duration.pipe';
+import { ProfileComponent } from './components/profile/profile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DurationPipe
+    DurationPipe,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
